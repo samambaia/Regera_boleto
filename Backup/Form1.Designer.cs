92f2542249d97441665b8d47a8014d9ede986c4c
samambaia <samambaia@ciandt.com>
2015-11-20 23:32:03 -0200

    Primeito commit para atualizar o remote

---
+++ Backup/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace RegeraBoleto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.sCodeBar1 = new System.Windows.Forms.TextBox();
+            this.sCodeBar2 = new System.Windows.Forms.TextBox();
+            this.sCodeBar3 = new System.Windows.Forms.TextBox();
+            this.sCodeBar4 = new System.Windows.Forms.TextBox();
+            this.sCodeBar5 = new System.Windows.Forms.TextBox();
+            this.sCodeBar6 = new System.Windows.Forms.TextBox();
+            this.sCodeBar7 = new System.Windows.Forms.TextBox();
+            this.sCodeBar8 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dt_vencimento = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo Barra:";
+            // 
+            // sCodeBar1
+            // 
+            this.sCodeBar1.Location = new System.Drawing.Point(26, 25);
+            this.sCodeBar1.Name = "sCodeBar1";
+            this.sCodeBar1.Size = new System.Drawing.Size(68, 20);
+            this.sCodeBar1.TabIndex = 1;
+            // 
+            // sCodeBar2
+            // 
+            this.sCodeBar2.Location = new System.Drawing.Point(100, 25);
+            this.sCodeBar2.Name = "sCodeBar2";
+            this.sCodeBar2.Size = new System.Drawing.Size(68, 20);
+            this.sCodeBar2.TabIndex = 2;
+            // 
+            // sCodeBar3
+            // 
+            this.sCodeBar3.Location = new System.Drawing.Point(174, 25);
+            this.sCodeBar3.Name = "sCodeBar3";
+            this.sCodeBar3.Size = new System.Drawing.Size(68, 20);
+            this.sCodeBar3.TabIndex = 3;
+            // 
+            // sCodeBar4
+            // 
+            this.sCodeBar4.Location = new System.Drawing.Point(248, 25);
+            this.sCodeBar4.Name = "sCodeBar4";
+            this.sCodeBar4.Size = new System.Drawing.Size(68, 20);
+            this.sCodeBar4.TabIndex = 4;
+            // 
+            // sCodeBar5
+            // 
+            this.sCodeBar5.Location = new System.Drawing.Point(322, 25);
+            this.sCodeBar5.Name = "sCodeBar5";
+            this.sCodeBar5.Size = new System.Drawing.Size(68, 20);
+            this.sCodeBar5.TabIndex = 5;
+            // 
+            // sCodeBar6
+            // 
+            this.sCodeBar6.Location = new System.Drawing.Point(396, 25);
+            this.sCodeBar6.Name = "sCodeBar6";
+            this.sCodeBar6.Size = new System.Drawing.Size(68, 20);
+            this.sCodeBar6.TabIndex = 6;
+            // 
+            // sCodeBar7
+            // 
+            this.sCodeBar7.Location = new System.Drawing.Point(470, 25);
+            this.sCodeBar7.Name = "sCodeBar7";
+            this.sCodeBar7.Size = new System.Drawing.Size(25, 20);
+            this.sCodeBar7.TabIndex = 7;
+            // 
+            // sCodeBar8
+            // 
+            this.sCodeBar8.Location = new System.Drawing.Point(501, 25);
+            this.sCodeBar8.Name = "sCodeBar8";
+            this.sCodeBar8.Size = new System.Drawing.Size(141, 20);
+            this.sCodeBar8.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Processar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Vencimento";
+            // 
+            // dt_vencimento
+            // 
+            this.dt_vencimento.Location = new System.Drawing.Point(26, 68);
+            this.dt_vencimento.Name = "dt_vencimento";
+            this.dt_vencimento.Size = new System.Drawing.Size(100, 20);
+            this.dt_vencimento.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 194);
+            this.Controls.Add(this.dt_vencimento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sCodeBar8);
+            this.Controls.Add(this.sCodeBar7);
+            this.Controls.Add(this.sCodeBar6);
+            this.Controls.Add(this.sCodeBar5);
+            this.Controls.Add(this.sCodeBar4);
+            this.Controls.Add(this.sCodeBar3);
+            this.Controls.Add(this.sCodeBar2);
+            this.Controls.Add(this.sCodeBar1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox sCodeBar1;
+        private System.Windows.Forms.TextBox sCodeBar2;
+        private System.Windows.Forms.TextBox sCodeBar3;
+        private System.Windows.Forms.TextBox sCodeBar4;
+        private System.Windows.Forms.TextBox sCodeBar5;
+        private System.Windows.Forms.TextBox sCodeBar6;
+        private System.Windows.Forms.TextBox sCodeBar7;
+        private System.Windows.Forms.TextBox sCodeBar8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox dt_vencimento;
+    }
+}
+
---
+++ Backup/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RegeraBoleto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int iFator_vencto = int.Parse(sCodeBar8.Text.Substring(0, 4));
+            int iFator_vencto2;
+            DateTime dDt_vencto1 = new DateTime(2003, 8, 8);
+            DateTime dDt_vencto2 = DateTime.Parse(dt_vencimento.Text);
+            int n_dias_atraso;
+            string dvGeral;
+            string valor = sCodeBar8.Text.Substring(4, 10);
+
+            //--->> fator 2131 => 08/08/2003
+
+            // Calcula a diferença em dias entre a data de 08/08/2003 e a nova data de vencimento
+
+            TimeSpan ts = new TimeSpan();
+            ts = dDt_vencto2 - dDt_vencto1;
+
+            int n_dias = int.Parse(ts.TotalDays.ToString());
+
+            // Calcula o novo fator de vencimento
+
+            iFator_vencto2 = 2131 + n_dias;
+            n_dias_atraso = iFator_vencto2 - iFator_vencto;
+
+            // Calcula o DV Geral
+
+            string fator = iFator_vencto2.ToString();
+            string campoLivre = sCodeBar1.Text.Substring(4, 1) + sCodeBar2.Text.Substring(0, 4) + sCodeBar3.Text + sCodeBar4.Text.Substring(0, 5) + sCodeBar5.Text + sCodeBar6.Text.Substring(0, 5);
+
+            dvGeral = DvMod11(sCodeBar1.Text.Substring(0, 4) + fator + valor + campoLivre);
+
+            // Novo Código de Barras
+
+            string codeBar = sCodeBar1.Text + "." + sCodeBar2.Text + "." + sCodeBar3.Text + "." + sCodeBar4.Text + "  " + sCodeBar5.Text + "." + sCodeBar6.Text + "   " + dvGeral + "  " + fator + valor;
+            label2.Text = codeBar;
+
+        }
+
+        /// <summary>
+        /// Calcula o DV módulo 11 de um número dado
+        /// </summary>
+        /// <param name="numero"></param>
+        /// <returns></returns>
+        public string DvMod11(string numero)
+        {
+            int i = numero.Length;
+            int fator = 2;
+            int total = 0;
+            int dv = 0;
+            while (i >= 1)
+            {
+                total += int.Parse(numero.Substring(i - 1, 1)) * fator;
+                fator += 1;
+                if (fator > 9)
+                    fator = 2;
+                i--;
+            }
+            dv = 11 - (total % 11);
+            if (dv >= 10)
+                return "1";
+            else
+                return dv.ToString();
+        }
+
+        /// <summary>
+        /// Replica uma string n vezes
+        /// </summary>
+        /// <param name="s">String a ser replicada</param>
+        /// <param name="n">Número de vezes que a string será replicada</param>
+        /// <returns>Retorna a string replicada. Ex: Replicate("0",5) => Retorna "00000"</returns>
+        protected string Replicate(string s, int n)
+        {
+            string x = "";
+            for (int i = 1; i <= n; i++)
+            {
+                x += s;
+            }
+            return x;
+        }
+
+        /// <summary>
+        /// Preenche com zeros a esquerda de uma string
+        /// </summary>
+        /// <param name="value">String que será preenchida com zeros a esquerda</param>
+        /// <param name="zeros">Quantidade de zeros que será colocado à esquerda de "value"</param>
+        /// <returns>Retorna a string preenchida com zeros a esquerda</returns>
+        public string Strzero(object value, int zeros)
+        {
+            string aux;
+            if (value == null)
+            {
+                aux = Replicate("0", zeros);
+                return aux;
+            }
+            else
+            {
+                aux = value.ToString();
+                return this.Replicate("0", zeros - aux.Length) + aux;
+            }
+        }
+
+    }
+}
